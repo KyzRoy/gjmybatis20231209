7a69f98d0619951f4111cb522449d09b8e68521a
KyzRoy <134759162+KyzRoy@users.noreply.github.com>
2023-12-09 16:05:31 +0800

    20231209 v1.0

---
+++ gjMybatis/src/main/java/com/example/demo/mapper/memberMapper.java
@@ -0,0 +1,29 @@
+package com.example.demo.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.example.demo.vo.member;
+
+@Mapper
+public interface memberMapper {
+	//create
+	@Insert("insert into member(name,username,password)"
+			+ "values(#{name},#{username},#{password})")
+			void add(member m);
+	
+	@Select("select * from member where username=#{username} and password=#{password}")
+	member queryMember(String username,String password);
+	
+	@Select("select * from member where username=#{username}")
+	member queryUsername(String username);
+	
+	//read
+	
+	
+	//update
+	
+	
+	//delete
+}
